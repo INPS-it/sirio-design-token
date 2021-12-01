35fa140e6c9830193f737b65b254a0d6be581dbb
Martina Carta <martina.carta@sketchin.com>
2021-12-01 16:41:13 +0100

    first design token commit

---
+++ developer/token/ios-swift/StyleDictionaryColor.swift
@@ -0,0 +1,99 @@
+
+//
+// StyleDictionaryColor.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 01 Dec 2021 11:42:20 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryColor {
+    public static let aliasBackgroundColorDisabled = UIColor(red: 0.776, green: 0.800, blue: 0.824, alpha: 1)
+    public static let aliasBackgroundColorPrimaryDark0 = UIColor(red: 0.000, green: 0.208, blue: 0.298, alpha: 1)
+    public static let aliasBackgroundColorPrimaryLight0 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let aliasBackgroundColorPrimaryLight1 = UIColor(red: 0.929, green: 0.957, blue: 0.969, alpha: 1)
+    public static let aliasGradientAccentPrimary = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let aliasGradientAccentPrimaryDark = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let aliasInteractiveAccentDefault = UIColor(red: 0.094, green: 0.808, blue: 0.906, alpha: 1)
+    public static let aliasInteractiveAccentFocus = UIColor(red: 0.075, green: 0.647, blue: 0.725, alpha: 1)
+    public static let aliasInteractiveAccentHover = UIColor(red: 0.549, green: 0.906, blue: 0.953, alpha: 1)
+    public static let aliasInteractiveAccentPressed = UIColor(red: 0.059, green: 0.490, blue: 0.541, alpha: 1)
+    public static let aliasInteractiveBorderFocus = UIColor(red: 0.271, green: 0.302, blue: 0.337, alpha: 1)
+    public static let aliasInteractivePrimaryDefault = UIColor(red: 0.000, green: 0.490, blue: 0.702, alpha: 1)
+    public static let aliasInteractivePrimaryFocus = UIColor(red: 0.000, green: 0.278, blue: 0.400, alpha: 1)
+    public static let aliasInteractivePrimaryHover = UIColor(red: 0.000, green: 0.349, blue: 0.502, alpha: 1)
+    public static let aliasInteractivePrimaryPressed = UIColor(red: 0.000, green: 0.208, blue: 0.298, alpha: 1)
+    public static let aliasInteractiveRedDefault = UIColor(red: 0.667, green: 0.133, blue: 0.310, alpha: 1)
+    public static let aliasInteractiveRedFocus = UIColor(red: 0.333, green: 0.067, blue: 0.157, alpha: 1)
+    public static let aliasInteractiveRedHover = UIColor(red: 0.498, green: 0.102, blue: 0.231, alpha: 1)
+    public static let aliasInteractiveRedPressed = UIColor(red: 0.247, green: 0.051, blue: 0.106, alpha: 1)
+    public static let aliasInteractiveSecondaryDefault = UIColor(red: 0.271, green: 0.302, blue: 0.337, alpha: 1)
+    public static let aliasInteractiveSecondaryFocus = UIColor(red: 0.090, green: 0.098, blue: 0.110, alpha: 1)
+    public static let aliasInteractiveSecondaryHover = UIColor(red: 0.176, green: 0.200, blue: 0.224, alpha: 1)
+    public static let aliasInteractiveSecondaryPressed = UIColor(red: 0.043, green: 0.051, blue: 0.055, alpha: 1)
+    public static let aliasTextColorDisabled = UIColor(red: 0.557, green: 0.596, blue: 0.643, alpha: 1)
+    public static let aliasTextColorPrimaryDark0 = UIColor(red: 0.000, green: 0.349, blue: 0.502, alpha: 1)
+    public static let aliasTextColorPrimaryLight0 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let aliasTextColorPrimaryLight1 = UIColor(red: 0.929, green: 0.957, blue: 0.969, alpha: 1)
+    public static let aliasTextColorSecondaryDark0 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let aliasTextColorSecondaryDark1 = UIColor(red: 0.271, green: 0.302, blue: 0.337, alpha: 1)
+    public static let data-entry-border-color-default = [object Object]
+    public static let data-entry-border-color-focus = [object Object]
+    public static let data-entry-border-color-hover = [object Object]
+    public static let data-entry-border-color-valued = [object Object]
+    public static let data-entry-label-color-default = [object Object]
+    public static let data-entry-label-color-focus = [object Object]
+    public static let data-entry-label-color-hover = [object Object]
+    public static let data-entry-label-color-valued = [object Object]
+    public static let data-entry-placeholder-color-default = [object Object]
+    public static let data-entry-placeholder-color-focus = [object Object]
+    public static let data-entry-placeholder-color-hover = [object Object]
+    public static let data-entry-placeholder-color-valued = [object Object]
+    public static let globalAccent100 = UIColor(red: 0.094, green: 0.808, blue: 0.906, alpha: 1)
+    public static let globalAccent110 = UIColor(red: 0.075, green: 0.647, blue: 0.725, alpha: 1)
+    public static let globalAccent120 = UIColor(red: 0.059, green: 0.490, blue: 0.541, alpha: 1)
+    public static let globalAccent70 = UIColor(red: 0.549, green: 0.906, blue: 0.953, alpha: 1)
+    public static let globalDarkPrimary110 = UIColor(red: 0.000, green: 0.349, blue: 0.502, alpha: 1)
+    public static let globalDarkPrimary115 = UIColor(red: 0.000, green: 0.278, blue: 0.400, alpha: 1)
+    public static let globalDarkPrimary120 = UIColor(red: 0.000, green: 0.208, blue: 0.298, alpha: 1)
+    public static let globalDarkPrimary130 = UIColor(red: 0.000, green: 0.071, blue: 0.102, alpha: 1)
+    public static let globalDarkSecondary110 = UIColor(red: 0.176, green: 0.200, blue: 0.224, alpha: 1)
+    public static let globalDarkSecondary120 = UIColor(red: 0.090, green: 0.098, blue: 0.110, alpha: 1)
+    public static let globalDarkSecondary125 = UIColor(red: 0.043, green: 0.051, blue: 0.055, alpha: 1)
+    public static let globalDarkSecondary130 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let globalGradient01 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let globalGradient02 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let globalLightPrimary40 = UIColor(red: 0.929, green: 0.957, blue: 0.969, alpha: 1)
+    public static let globalLightPrimary50 = UIColor(red: 0.761, green: 0.886, blue: 0.941, alpha: 1)
+    public static let globalLightPrimary60 = UIColor(red: 0.549, green: 0.831, blue: 0.949, alpha: 1)
+    public static let globalLightSecondary40 = UIColor(red: 0.890, green: 0.898, blue: 0.910, alpha: 1)
+    public static let globalLightSecondary50 = UIColor(red: 0.776, green: 0.800, blue: 0.824, alpha: 1)
+    public static let globalLightSecondary60 = UIColor(red: 0.667, green: 0.698, blue: 0.733, alpha: 1)
+    public static let globalMidPrimary70 = UIColor(red: 0.510, green: 0.706, blue: 0.788, alpha: 1)
+    public static let globalMidPrimary80 = UIColor(red: 0.278, green: 0.659, blue: 0.820, alpha: 1)
+    public static let globalMidPrimary90 = UIColor(red: 0.090, green: 0.596, blue: 0.812, alpha: 1)
+    public static let globalMidSecondary70 = UIColor(red: 0.557, green: 0.596, blue: 0.643, alpha: 1)
+    public static let globalMidSecondary80 = UIColor(red: 0.447, green: 0.498, blue: 0.553, alpha: 1)
+    public static let globalMidSecondary90 = UIColor(red: 0.357, green: 0.396, blue: 0.443, alpha: 1)
+    public static let globalPrimary000 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let globalPrimary100 = UIColor(red: 0.000, green: 0.490, blue: 0.702, alpha: 1)
+    public static let globalSecondary100 = UIColor(red: 0.271, green: 0.302, blue: 0.337, alpha: 1)
+    public static let globalSecondary90 = UIColor(red: 0.357, green: 0.404, blue: 0.447, alpha: 1)
+    public static let globalSemanticAlert100 = UIColor(red: 0.667, green: 0.133, blue: 0.310, alpha: 1)
+    public static let globalSemanticAlert110 = UIColor(red: 0.498, green: 0.102, blue: 0.231, alpha: 1)
+    public static let globalSemanticAlert120 = UIColor(red: 0.333, green: 0.067, blue: 0.157, alpha: 1)
+    public static let globalSemanticAlert130 = UIColor(red: 0.247, green: 0.051, blue: 0.106, alpha: 1)
+    public static let globalSemanticInfo100 = UIColor(red: 0.000, green: 0.467, blue: 0.639, alpha: 1)
+    public static let globalSemanticSuccesso100 = UIColor(red: 0.016, green: 0.596, blue: 0.580, alpha: 1)
+    public static let globalSemanticWarning100 = UIColor(red: 0.886, green: 0.388, blue: 0.204, alpha: 1)
+    public static let option-background-color-focus = [object Object]
+    public static let option-background-color-hover = [object Object]
+    public static let option-background-color-pressed = [object Object]
+    public static let option-background-color-valued = [object Object]
+    public static let specificDataEntryBorderColorError = [object Object]
+    public static let specificDataEntryBorderColorSuccess = [object Object]
+    public static let specificDataEntryBorderColorWarning = [object Object]
+    public static let specificProgressbarBackgroundColor = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+}
